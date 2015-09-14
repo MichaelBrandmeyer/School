6e0fd8894e232a4c9d332082070648dc8045df81
unknown <373000828@mvhsrop901-07.vusdnoc.vusd.k12.ca.us>
2015-09-14 14:35:23 -0700

    Initial Commit

---
+++ src/school/Course.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package school;
+
+/**
+ *
+ * @author 373000828
+ */
+public class Course {
+    
+}
