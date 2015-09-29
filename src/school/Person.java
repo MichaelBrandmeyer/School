b49a07c36106a118807caaea6aaa625ad737c169
unknown <373000828@mvhsrop901-19.vusdnoc.vusd.k12.ca.us>
2015-09-29 13:33:23 -0700

    Initial Commit

--- src/school/Person.java
+++ src/school/Person.java
@@ -1,99 +1,104 @@
-
 package school;
 import java.util.ArrayList;
-public class Person {
-//    
-//    public static int numPeople = 10;
-//    public static int currentPeopleIndex = 6;
-//    private static Person people[] = new Person[numPeople];
-    private static ArrayList<Person> people =
-            new ArrayList<Person>();
-    
-    enum Gender{
+import java.util.Calendar;
+public class Person {      
+    protected static ArrayList<Person> people = 
+    new ArrayList<Person>();
+    enum Gender {
         Male,Female
     }
-    
-    private Gender gender;
-    private String name;
+    private String name; 
     private int weight;
-    
-    
+    private Gender gender;
+    private int birthDay;
+    private int birthMonth;
+    private int birthYear;
+
     public static Person addPerson(String _name,
     Gender _gender, int _weight)
     {
-        Person temp = new Person(_name, _gender, _weight);
-//        people[currentPeopleIndex++] = temp;
+        Person temp = new Person(_name,_gender,_weight);
         people.add(temp);
         return(temp);
-    }
-    
-
-    
-    
+    }    
+    public static void addPerson(Person _person)
+    {
+        people.add(_person);
+    }    
     Person()
     {
-        name = "Joe";
+        name = "None";
         weight = 100;
-        gender = gender.Male;
+        gender = Gender.Female;
     }
-    Person(String _name, Gender _gender, int _weight)
+    Person(String _name,Gender _gender,int _weight)
     {
         name = _name;
         weight = _weight;
         gender = _gender;
-        
+    }  
+    public void setBirthdate
+    (int _day,int _month,int _year)
+    {
+        birthDay = _day;
+        birthMonth = _month;
+        birthYear = _year;
+          
     }
-
+    public int getAge()
+    {
+        Calendar now = Calendar.getInstance();
+        int day = now.get(Calendar.DAY_OF_MONTH);
+        int month = now.get(Calendar.MONTH) + 1;
+        int year = now.get(Calendar.YEAR);
+        return(0);
+    }    
+    public void setWeight(int _weight)
+    {
+        weight = _weight;
+    }
     public int getWeight()
     {
         return(weight);
-    }
-    public String getName()
-    {
-        return(name);
-    }
-    public Gender getGender()
-    {
-        return(gender);
-    }
-    public void setWeight(int _weight)
-    {
-       weight = _weight;
-    }
+    }       
     public void setName(String _name)
     {
         name = _name;
     }
+    public String getName()
+    {
+        return(name);
+    }    
     public void setGender(Gender _gender)
     {
         gender = _gender;
     }
+    public Gender getGender()
+    {
+        return(gender);
+    }      
     
     public static void printNames()
     {
-      /*  for (int i=0; i < people.length;i++)
+        System.out.println("===printNames===");
+        for (Person temp : people)
         {
-            if (people[i] != null)
-                System.out.println(people[i].getName());
-        } 
-    }*/
-    
-       for (Person temp : people)
-        System.out.println(temp.getName());            
-        
-  }
+            System.out.println(temp.getName());
+        }
+    }    
     public static void printNames(Gender _gender)
     {
         System.out.println("===printNames===");
         for (Person temp : people)
-            
-        if (temp.gender == _gender)
-        System.out.println(temp.getName()); 
+        {
+            if (temp.gender == _gender)
+                System.out.println(temp.getName());
+        }
     }
+    
+    
     public String toString()
     {
-        return(name +" "+ gender +" "+ weight);
-    }
+        return(name + " " + gender + " " + weight);
+    }    
 }
-    
-
